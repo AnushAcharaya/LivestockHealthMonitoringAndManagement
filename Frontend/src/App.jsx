f31b463e170d5ae37198a7f75c5d593268277e98
Anush <np03cs4a230034@heraldcollege.edu.np>
2025-11-23 20:55:43 +0545

    frontend setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <h1 className="bg-black text-white text-4xl p-6 rounded-xl">
+        Hello World
+      </h1>
+    </div>
+
+
+  )
+}
+
+export default App
